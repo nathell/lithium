74db42bcb1d60c902a36021c33022d76e9e96086
Daniel Janus <dj@danieljanus.pl>
2012-12-27 17:19:46 +0000

    Upgrade to Clojure 1.4, drop dependency on tools.cli.

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject lithium "0.0.1"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.cli "0.2.1"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
