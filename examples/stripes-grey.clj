615ca4bc40dccf2ad31940bfc27879450f8f0209
Daniel Janus <dj@danieljanus.pl>
2013-01-16 08:58:50 +0000

    Stripes in Clojure!

---
+++ examples/stripes-grey.clj
@@ -0,0 +1,9 @@
+(do (init-graph) 
+    (loop [x 0 y 0]
+      (put-pixel x y (let [z (mod (+ x y) 32)]
+                       (if (< z 16) (+ 16 z) (+ 16 (- 31 z))))) 
+      (if (= y 200)
+        nil
+        (if (= x 319)
+          (recur 0 (inc y))
+          (recur (inc x) y)))))
